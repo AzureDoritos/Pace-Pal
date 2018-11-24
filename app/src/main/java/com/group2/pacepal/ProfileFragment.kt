ca546565c48d197e50326d08753e6ad42c426784
unknown <mcj108@outlook.com>
2018-11-23 21:03:39 -0800

    Can now read user profile data
    Addressed #14

--- app/src/main/java/com/group2/pacepal/ProfileFragment.kt
+++ app/src/main/java/com/group2/pacepal/ProfileFragment.kt
@@ -5,11 +5,77 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
+//import kotlinx.android.synthetic.main.user_profile
+import kotlinx.android.synthetic.main.user_profile.*
+//import jdk.nashorn.internal.runtime.ECMAException.getException
+//import android.support.test.orchestrator.junit.BundleJUnitUtils.getResult
+//import org.junit.experimental.results.ResultMatchers.isSuccessful
+import com.google.firebase.auth.FirebaseAuth
+//import jdk.nashorn.internal.runtime.ECMAException.getException
+//import android.support.test.orchestrator.junit.BundleJUnitUtils.getResult
+import com.google.firebase.firestore.DocumentSnapshot
+//import org.junit.experimental.results.ResultMatchers.isSuccessful
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+
+
+
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.user_profile, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val db = FirebaseFirestore.getInstance()
+        val user = FirebaseAuth.getInstance().currentUser
+
+
+        //findViewByID(R.id.profileUsername) =
+
+        //val tv = findViewById(R.id.profileUsername) as TextView
+        //tv.text = "this string is set dynamically from java code"
+
+        val useruid = user!!.uid
+
+        val docRef = db.collection("users").document(useruid)
+        docRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val currentProfile = task.result
+                if (currentProfile!!.exists()) {
+                    //Log.d(TAG, "DocumentSnapshot data: " + document.data!!)
+                    profileUsername.text = currentProfile.getString("username")
+                    profileMiles.text = currentProfile.getDouble("miles").toString()
+                    profileChallenges.text = currentProfile.get("challenges").toString()
+
+
+
+                    db.collection("users").document(useruid).collection("friends:").get().addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            profileFriends.text = task.result!!.size().toString()
+
+                        } else {
+                            profileFriends.text = "Na"
+                        }
+                    }
+
+
+                } else {
+                    profileUsername.text = "NaNa"
+                    profileMiles.text = "NaNa"
+                    profileChallenges.text = "NaNa"
+                }
+            } else {
+                profileUsername.text = "Na"
+                profileMiles.text = "Na"
+                profileChallenges.text = "Na"
+            }
+        }
+
+
+
+        return inflater.inflate(R.layout.user_profile, container, false)
+    }
 
     companion object {
         fun newInstance(): ProfileFragment = ProfileFragment()
