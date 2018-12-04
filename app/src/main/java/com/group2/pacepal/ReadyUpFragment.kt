c6526073929c62c3fcdc4ff47fe97a08b21725d4
unknown <mcj108@outlook.com>
2018-12-04 02:53:01 -0800

    Beginning code for ready up stage of joining session

--- app/src/main/java/com/group2/pacepal/ReadyUpFragment.kt
+++ app/src/main/java/com/group2/pacepal/ReadyUpFragment.kt
@@ -1,8 +1,77 @@
 package com.group2.pacepal
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.readyup_fragment.*
 
 
 class ReadyUpFragment : Fragment() {
-    //
+
+    private val user = FirebaseAuth.getInstance().currentUser
+    private val userid = user!!.uid
+    val sessionID = arguments!!.getString("sessionID")
+    private val rtdb = FirebaseDatabase.getInstance().reference
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var hostReady = false
+        var p2Ready = false
+        var absReady = false
+
+        val hostListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                if(dataSnapshot.child("p2ready").toString() == "true") {
+                    p2Ready = true
+                    palStatus.text = "Ready"
+                    //palStatus.setTextColor(#ff669900) = #ff669900
+                }
+                else{
+                    p2Ready = false
+                    palStatus.text = "Not Ready"
+                }
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("loadPost:onCancelled ${databaseError.toException()}")
+            }
+        }
+
+        val p2Listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                if(dataSnapshot.child("ready").value.toString() == userid) {
+
+
+
+                }}
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("loadPost:onCancelled ${databaseError.toException()}")
+            }
+        }
+
+        if(userid == sessionID)
+            rtdb.child("sessionManager").child("sessionIndex").child(sessionID).child("ready").addValueEventListener(hostListener)
+        else
+            rtdb.child("sessionManager").child("sessionIndex").child(sessionID).addValueEventListener(p2Listener)
+
+
+
+        return inflater.inflate(R.layout.readyup_fragment, container, false)
+        }
+
+    companion object {
+        fun newInstance(): ReadyUpFragment = ReadyUpFragment()
+    }
+
 }
--- app/src/main/java/com/group2/pacepal/SessionActivity.kt
+++ app/src/main/java/com/group2/pacepal/SessionActivity.kt
@@ -1,17 +1,36 @@
 package com.group2.pacepal
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 
+
 class SessionActivity : AppCompatActivity() {
+
+    val sessionId = intent.getStringExtra("sessionID")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.session_activity)
 
-        val sessionId = intent.getStringExtra("sessionID")
+        val transaction = supportFragmentManager.beginTransaction()
+        val bundle = Bundle()
+        bundle.putString("message", sessionId)
+        val fragInfo = Fragment()
+        fragInfo.arguments = bundle
+        transaction.replace(R.id.heldFragment, fragInfo)
+        transaction.commit()
+
+
+
+
+
+
+
 
 
 
     }
+
+
 }
