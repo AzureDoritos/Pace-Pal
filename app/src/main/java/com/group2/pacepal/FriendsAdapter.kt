86331af06f2f418c5ea05c631b73ad09ad1edbf4
aaronm-2112 <aaron.marroquin96@gmail.com>
2018-12-09 16:01:54 -0800

    Friend's list now has a working friendProfileFragment

--- app/src/main/java/com/group2/pacepal/FriendsAdapter.kt
+++ app/src/main/java/com/group2/pacepal/FriendsAdapter.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.preference.PreferenceManager
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
@@ -18,9 +19,15 @@ import com.mapbox.mapboxsdk.Mapbox.getApplicationContext
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.user_profile.view.*
 import java.security.AccessController.getContext
+import android.os.Bundle
+
+
 
 
 internal class FriendsAdapter constructor (private var friends: ArrayList<Friend>): RecyclerView.Adapter<FriendsAdapter.FriendHolder>() {
+
+    //val friendAdaptorCommunication = com.group2.pacepal.friendAdaptorCommunication
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendHolder{
         val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.friendview_row_item,parent,false)
@@ -69,7 +76,28 @@ internal class FriendsAdapter constructor (private var friends: ArrayList<Friend
 
             view.friendSelection.setOnClickListener{
                 if(friend.activityType == 1){
-                    Toast.makeText(friend.feature,"TODO: open friend profile", Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(friend.feature,"TODO: open friend profile", Toast.LENGTH_SHORT).show()
+                    //openFragment(friendProfileFragment.newInstance())
+                    //val preferences = PreferenceManager.getDefaultSharedPreferences(friend.feature.applicationContext)
+                    //val editor = preferences.edit()
+                   // editor.putString("friend_profile", friend.uid) //experiment here
+                   // editor.commit()
+
+
+                    val myFragment = friendProfileFragment()
+                    val bundle = Bundle()
+                    bundle.putString("friend_uid", friend.uid)
+                    bundle.putString("friend_userName", friend.userName)
+                    bundle.putString("friend_real_name", friend.realName)
+                    myFragment.setArguments(bundle)
+                    //refers to the primary activity's (the mainActivity2) context to create a new fragement within a fragment and still
+                    //keep track of the fragemnt's position on the stack
+                    //potentially buggy so we can take more looks at this in the future.
+                    val activity = view.context as AppCompatActivity
+                    activity.supportFragmentManager.beginTransaction().replace(R.id.container, myFragment).addToBackStack(null).commit() //test
+
+
+
                 }else if(friend.activityType == 2){
 
 
@@ -121,4 +149,9 @@ internal class FriendsAdapter constructor (private var friends: ArrayList<Friend
             }*/
         }
     }
+
+
+
 }
+
+
