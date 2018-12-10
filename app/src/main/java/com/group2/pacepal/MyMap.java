5589af0059169d637e306a4aa7dad67925eccdec
felimr <33796464+felimr@users.noreply.github.com>
2018-12-09 22:15:50 -0800

    Update MyMap.java for map to show user location
    Added the necessary code for the map to show the users location while using the map.

--- app/src/main/java/com/group2/pacepal/MyMap.java
+++ app/src/main/java/com/group2/pacepal/MyMap.java
@@ -1,32 +1,137 @@
 package com.group2.pacepal;
 
 
+import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+
+import java.util.List;
 
 
 
 public class MyMap extends AppCompatActivity {
     private MapView mapView;
-
+    //location code
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, getString(R.string.access_token));
+        Mapbox.getInstance(context:this, getString(R.string.access_token));
         setContentView(R.layout.my_map);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(callback:this );
     }
 
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+    }
+    //Asks for users permission to access GPS
+    private void enableLocation(){
+        if (PermissionsManager.areLocationPermissionsGranted(context:this)){
+            initializeLocationEngine();
+            initializeLocationLayer();
+        } else {
+            permissionsManager = new PermissionsManager(listener:this);
+            permissionsManager.requestLocationPermissions(activity:this);
+        }
+    }
+    @SuppressWarnings("MissingPermssion")
+    private void initializeLocationEngine(){
+        locationEngine = new LocationEngineProvider(context:this).obtainBestLocationEngineAvailable();
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if(lastLocation != null){
+            originLocation = lastLocation;
+            setCameraPosition(lastLocation);
+        } else{
+            locationEngine.addLocationEngineListener(this);
+        }
+
+
+    }
+
+    @SuppressWarnings("MissingPermssion")
+    private void initializeLocationLayer(){
+        locationLayerPlugin = new LocationLayerPlugin(mapView, map, locationEngine);
+        locationLayerPlugin.setLocationLayerEnabled(true);
+        locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+        locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+    }
+    //Sets camera location to a specific point on map and zooms the location i
+    private void setCameraPosition(Location location){
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),
+                location.getLongitude()),  zoom:13.0 ));
+    }
+    @Override
+    @SuppressWarnings("MissingPermssion")
+    public void onConnected() {
+        locationEngine.requestLocationUpdates();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null){
+            originLocation = location;
+            setCameraPosition(location);
+        }
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        //
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        //Enables location once it has been permitted by user
+        if (granted){
+            enableLocation();
+        }
+
+    }
+    //permissions code
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       permissionsManager.onRequestPermissionsResult(requestCode, permissions,grantResults);
+    }
+    
+    
+    
+    
     @Override
     public void onStart() {
         super.onStart();
