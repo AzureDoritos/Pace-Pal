e9508ca0a86db5ab7a5b0ade8cc474f52e264c90
Mason <mcj108@outlook.com>
2018-12-11 00:45:46 -0800

    Location works!!!!!!!!

--- app/src/main/java/com/group2/pacepal/MyMap.java
+++ app/src/main/java/com/group2/pacepal/MyMap.java
@@ -2,14 +2,41 @@ package com.group2.pacepal;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.core.MapboxService;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
@@ -18,10 +45,19 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 
-import com.mapbox.mapboxsdk.maps.MapView;
+import timber.log.Timber;
 
-public class MyMap extends AppCompatActivity {
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+public class MyMap extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private MapView mapView;
+
+    private double locLong;
+    private double locLat;
+
+    private GoogleApiClient googleApiClient;
+    private LocationRequest mLocationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,54 +69,134 @@ public class MyMap extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
 
 
+        //Instantiating the GoogleApiClient
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        startLocationUpdates();
 
 
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                // One way to add a marker view
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(41.885, -87.679))
-                        .title("Chicago")
-                        .snippet("Illinois")
+        Handler handler = new Handler();
+        int delay = 2000; //milliseconds
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                MarkerOptions marker2 = new MarkerOptions()
+                        .position(new LatLng(locLat, locLong))
+                        .title("Player1")
+                        .snippet("Player1loc");
 
 
-                );
-                LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                locationComponent.activateLocationComponent(getBaseContext());
-                locationComponent.setLocationComponentEnabled(true);
+
+
+
+
+                mapView.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(MapboxMap mapboxMap) {
+
+                        mapboxMap.clear();
+                        //mapboxMap.addMarker(marker1);
+                        mapboxMap.addMarker(marker2);
+
+                        Log.d("MapRefresh", "refreshed");
+
+                        mapboxMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(41.885, -87.679))
+                                .title("Chicago")
+                                .snippet("Illinois")
+                        );
+                    }
+                });
+                handler.postDelayed(this, delay);
             }
-        });
-
-
-
-
-
+        }, delay);
     }
 
-
-
-
-
-
-    @Override
     public void onStart() {
         super.onStart();
-        mapView.onStart();
+        // Initiating the connection
+        googleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+        // Disconnecting the connection
+        googleApiClient.disconnect();
+    }
+
+    //Callback invoked once the GoogleApiClient is connected successfully
+    @Override
+    public void onConnected(Bundle bundle) {
+        //Fetching the last known location using the FusedLocationProviderApi
     }
 
     @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    //Callback invoked if the GoogleApiClient connection fails
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1500);
+        mLocationRequest.setFastestInterval(1500);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        //LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        locLat = location.getLatitude();
+        locLong = location.getLongitude();
+    }
+
+
+       @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
@@ -92,11 +208,7 @@ public class MyMap extends AppCompatActivity {
         mapView.onPause();
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
+
 
     @Override
     public void onLowMemory() {
@@ -116,66 +228,9 @@ public class MyMap extends AppCompatActivity {
         mapView.onSaveInstanceState(outState);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    //Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
 
 
 
-    public void onMapReady(MapboxMap mapboxMap) {
-
-        LocationComponent locationComponent = mapboxMap.getLocationComponent();
-
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-            locationComponent.activateLocationComponent(this);
-        }
 
 
-        locationComponent.setLocationComponentEnabled(true);
-
-    }
 }
